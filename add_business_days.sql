d1c3f08aa7cbe401b6e7a43ef03f074fb1f97b70
evohnave <evohnave@users.noreply.github.com>
2023-09-29 13:01:14 -0400

    Create add_business_days.sql

---
+++ add_business_days.sql
@@ -0,0 +1,24 @@
+/*
+This function determines the date of the start date plus a number of business
+  days.  The number of business days must be positive (i.e., looking for a
+  date in the future).
+
+Usage:
+
+SELECT   *
+  FROM   ADD_BUSINESS_DAYS('2023-09-01'::DATE, 5)
+;
+
+*/
+CREATE  OR REPLACE FUNCTION ADD_BUSINESS_DAYS(start_date DATE, num_days BIGINT)
+        RETURNS DATE
+        AS $ABD$
+           SELECT  d::DATE
+             FROM  GENERATE_SERIES(start_date,
+                                   start_date + num_days * 2 * INTERVAL '1d',
+                                   '1 day'::INTERVAL) d
+            WHERE EXTRACT('dow' FROM d) NOT IN (0, 6)
+            LIMIT 1
+            OFFSET 5
+        $ABD$ LANGUAGE SQL
+;
