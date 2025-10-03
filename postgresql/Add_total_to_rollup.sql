0b31b57238b8049b625bfaeba9f6bb78e366d423
evohnave <evohnave@users.noreply.github.com>
2025-10-03 10:12:59 -0400

    Update Add_total_to_rollup.sql

--- postgresql/Add_total_to_rollup.sql
+++ postgresql/Add_total_to_rollup.sql
@@ -6,7 +6,7 @@ SELECT   CASE
            WHEN GROUPING(rc.descr) = 1 THEN 'Total'
            ELSE rc.descr
          END "Descr"
-         , COUNT(*) "Count"
+         , COUNT(*) 'Count'
   FROM   s_table s
   JOIN   rc_table rc
     ON   rc.rc_id = s.rc_id
