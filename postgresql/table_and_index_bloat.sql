2b8a7b1f568464ba19e243e1fe1a89dd4dee2132
Eric <eric.vanhove@gmail.com>
2026-04-26 11:20:55 -0400

    Added pgstattuple example

---
+++ postgresql/table_and_index_bloat.sql
@@ -0,0 +1,10 @@
+-- Requires the pgstattuple extension
+-- CREATE EXTENSION IF NOT EXISTS pgstattuple;
+
+SELECT   relname AS "Table Name"
+       , PG_SIZE_PRETTY(PG_RELATION_SIZE(relid)) AS "Table Size"
+       , (pgstattuple(relid)).dead_tuple_percent AS "Pct Dead"
+       , (pgstattuple(relid)).free_percent AS "Pct Free"
+  FROM   pg_catalog.pg_stat_user_tables
+ ORDER   BY dead_pct DESC
+;
